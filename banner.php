e32a6e4b6c9848d6a76e8a8f6d611cdb707b920e
Trevor <trevor@Chaos-mac.local>
2013-12-16 19:35:43 +0300

    added breadcrumbs and page header

--- banner.php
+++ banner.php
@@ -1,6 +1,19 @@
 <?php if (has_post_thumbnail( $post->ID ) ): ?>
 	<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 	<div class="banner inner" style="background-image: url('<?php echo $image[0]; ?>')">
-		<h2 class="title"> <?php echo wp_title(''); ?></h2>
+		<div class="page-title">
+			<h2 class="title"> <?php echo wp_title(''); ?></h2>
+			<?php 
+				if ( function_exists( 'breadcrumb_trail' ) ) {
+					$defaults = array(
+						'container'       => 'div',   // container element
+						'separator'       => '', // separator between items
+						'before'          => '',      // HTML to output before
+						'after'           => '',      // HTML to output after
+					);
+					breadcrumb_trail($defaults); 
+				}
+			?>
+		</div>
 	</div>
 <?php endif; ?>
