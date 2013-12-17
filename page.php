434511216c2c4c5563933f3af53fa505ea68ea4f
Trevor <trevor@Chaos-mac.local>
2013-12-17 11:16:02 +0300

    Added single page template

---
+++ page.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ */
+
+get_header(); ?>
+
+<!-- main page content -->
+<div class="main">
+
+	<!-- Inner Page Banner -->
+	<?php get_template_part( 'banner' ); ?>
+
+</div>
+
+<?php get_footer(); ?>
